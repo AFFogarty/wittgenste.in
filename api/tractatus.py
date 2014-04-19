909d838940e38fdd94f8100f142b680adc83017c
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2014-04-19 18:45:24 -0400

    removed printing

--- api/tractatus.py
+++ api/tractatus.py
@@ -9,15 +9,15 @@ class Tractatus:
 
         # Build the Book object
         self.tractatus = Book()
-        print "Tractatus initialized"
+        # print "Tractatus initialized"
 
         # Get the tractatus from text
         all_text = open('static/tractatus.txt', 'r')
         output = all_text.readlines()
 
-        print "text file downloaded"
+        # print "text file downloaded"
 
-        print len(output)
+        # print len(output)
 
         # Read the file line by line into self.tractatus
         # The actual content only begins at line 30 until the end
@@ -32,7 +32,7 @@ class Tractatus:
             # Load them in
             self.tractatus.add_section(index, text)
 
-        print "loop complete"
+        # print "loop complete"
         print self.tractatus
 
     def get_book(self):
