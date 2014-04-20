e9dd23f2f6b577879c519489c9eb214d1c130641
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2014-04-20 15:05:33 -0400

    Fixed security blocking issue

--- api/wittapi.py
+++ api/wittapi.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
+from flask_cors import cross_origin
 from tractatus import Tractatus
 
 
 app = Flask(__name__)
+
+# Set the correct origin
+app.config['CORS_ORIGINS'] = ['wittgenste.in']
 
 @app.route("/")
 def hello():
     return "Visit /random for a random passage from the Tractatus."
 
 @app.route("/random")
-def random():
+@cross_origin() # allow all origins all methods.
+def random(methods = ['GET']):
     # Build the Tractatus and get a random section
     # print "Build tractatus"
     tractatus = Tractatus()
@@ -23,11 +28,11 @@ def random():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return 'Error 404: What we cannot speak about we must pass over in silence.', 404
+    return make_response(jsonify( { 'error': 'Error 404: What we cannot speak about we must pass over in silence.' } ), 404)
 
 @app.errorhandler(500)
 def server_error(error):
-    return 'Error 500: Something went wrong behind the scenes...', 500
+    return make_response(jsonify( { 'error': 'Error 500: Something went wrong behind the scenes...' } ), 500)
 
 if __name__ == "__main__":
     app.run()
